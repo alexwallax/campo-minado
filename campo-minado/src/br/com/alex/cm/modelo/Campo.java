4078031094abb72ca86cda3921f91e93ac98ac5b
Alex Wallace <alexwallax@gmail.com>
2023-05-25 15:33:51 -0300

    teste

--- campo-minado/src/br/com/alex/cm/modelo/Campo.java
+++ campo-minado/src/br/com/alex/cm/modelo/Campo.java
@@ -1,5 +1,6 @@
 package br.com.alex.cm.modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Campo {
@@ -16,6 +17,10 @@ public class Campo {
 	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		
 	}
 
 }
