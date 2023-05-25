c13b3deab7a1c057e7002281d42e5f086a4f3a06
Alex Wallace <alexwallax@gmail.com>
2023-05-25 15:21:52 -0300

    commit classe Campo 01

--- campo-minado/src/br/com/alex/cm/modelo/Campo.java
+++ campo-minado/src/br/com/alex/cm/modelo/Campo.java
@@ -1,5 +1,17 @@
 package br.com.alex.cm.modelo;
 
 public class Campo {
+	
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto = false;
+	private boolean mionado = false;
+	private boolean marcado = false;
+	
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
 
 }
--- campo-minado/src/br/com/alex/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/alex/cm/modelo/Tabuleiro.java
@@ -1,7 +1,6 @@
 package br.com.alex.cm.modelo;
 
 public class Tabuleiro {
-public Tabuleiro() {
-	// TODO Auto-generated constructor stub
-}
+
+	
 }
