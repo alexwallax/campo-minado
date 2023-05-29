b911c575b9ce7054190603f4f47a0eab5dfd9428
Alex Wallace <alexwallax@gmail.com>
2023-05-28 21:07:19 -0300

    commit tabuleiro 02

--- campo-minado/src/br/com/alex/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/alex/cm/modelo/Tabuleiro.java
@@ -3,6 +3,7 @@ package br.com.alex.cm.modelo;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
@@ -39,6 +40,27 @@ public class Tabuleiro {
 	}
 
 	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+		} while(minasArmadas < minas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
 		
 	}
 	
